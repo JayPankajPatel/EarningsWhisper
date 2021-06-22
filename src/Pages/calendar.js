a274a2c21cd45b98050238cca136978656165392
Kushal Bhandari <43105808+KuzscoTech@users.noreply.github.com>
2021-06-22 16:50:23 -0700

    Revert "EarningsBar Added"
    This reverts commit 433f83661b5242dfc8ddec053bc50dbd5dba23d4.

--- src/Pages/calendar.js
+++ src/Pages/calendar.js
@@ -9,8 +9,6 @@ import {
 } from "react-native";
 import TopBar from "../../components/TopBar";
 import Transactions from "../../components/Transactions";
-import EarningsBar from "../../components/EarningsBar";
-import { LinearGradient } from "expo-linear-gradient";
 function calendar({ navigation, profileName }) {
   return (
     <View style={{ top: 25, backgroundColor: "#F5ECDE", flex: 1 }}>
@@ -34,11 +32,7 @@ function calendar({ navigation, profileName }) {
         </ImageBackground>
       </View>
       {/* Calendar Stuff */}
-      <View style={styles.calendarStuff}>
-       
-       
-      </View>
-
+      <View style={styles.calendarStuff}></View>
       {/*Transactions */}
       <View style={styles.transactions}></View>
     </View>
@@ -51,7 +45,6 @@ const styles = StyleSheet.create({
   },
   calendarStuff: {
     flex: 3,
-
   },
   border: {
     backgroundColor: "#F5ECDE",
@@ -80,7 +73,6 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
     justifyContent: "center",
   },
-  
 });
 
 export default calendar;
